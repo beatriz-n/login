eb054ce6ea4fd7cefe24ae34098005b49c8152a5
beatriz <beatriznespolid@gmail.com>
2022-08-16 14:37:57 -0300

    style login

---
+++ prod/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app>
+    <v-main class="body">
+      <v-sheet class="box-container"
+  height="420"
+  width="270"
+>
+<v-sheet
+  color="white"
+  height="300"
+  width="250"
+>
+<CamposComp></CamposComp>
+</v-sheet>
+<v-sheet class="box-bottom"
+  color="white"
+  height="60"
+  width="250"
+>
+<p><a href="#"> Cadastre-se</a></p>
+</v-sheet>
+</v-sheet>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import CamposComp from './components/camposComp.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    CamposComp
+  },
+
+  data: () => ({
+    //
+  })
+}
+</script>
+<style scoped>
+.v-sheet{
+  margin-left: 3.4%;
+  margin-top: 3%;
+  padding-top: 1%;
+   border:solid rgba(216,216,216,255) 1.3px;
+    border-radius: 0.1rem;
+}
+.box-bottom{
+  margin-top: 1rem;
+  margin-bottom: 2rem;
+}
+a{
+  /* color: rgb(106, 193, 244); */
+  color: rgb(253, 127, 127);
+}
+p{
+  text-align: center;
+  font-size: small;
+  padding-top: 1.3rem;
+}
+.box-container{
+  background-color:rgba(250,250,250,255);
+  margin-left: 40%;
+}
+.body{
+  background-color: rgba(245,245,245,255);
+}
+</style>
---
+++ prod/src/components/camposComp.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="register">
+    <div class="form">
+      <v-container class="grey lighten-5">
+        <v-row no-gutters>
+          <v-row class="pa-1" :key="n">
+            <v-row class="pa-2">
+              <img
+                src="http://congressointereduca.web249.uni5.net/certificados/img/tsuru-origami.png"
+              />
+              <div class="space">
+                <h1>Sistema</h1>
+              </div>
+            </v-row>
+          </v-row>
+        </v-row>
+      </v-container>
+      <br />
+      <v-divider>ou</v-divider>
+      <div class="input-form">
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+      </div>
+      <v-btn class="btn-register">Login</v-btn>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'camposComp'
+}
+</script>
+<style scoped>
+.space {
+  padding-left: 1rem;
+  display: flex;
+  align-content: center;
+}
+.register {
+  align-items: center;
+}
+h1 {
+  color: black;
+  margin-bottom: 1rem;
+}
+h5 {
+  color: rgb(163, 163, 163);
+  margin-right: 30px;
+  margin-left: 30px;
+}
+.form {
+  text-align: center;
+  margin-top: 2rem;
+}
+.text-register,
+b {
+  color: rgb(163, 163, 163);
+  font-size: 10px;
+  margin-right: 25px;
+  margin-left: 25px;
+  display: inline-table;
+}
+.v-btn {
+  display: flex;
+  /* background-color: rgba(1,149,247,255); */
+  background-color: rgb(205, 40, 40);
+  color: aliceblue;
+  height: 1.5rem;
+  margin-left: 10px;
+  font-size: 10px;
+  width: 14rem;
+  margin-top: 1rem;
+}
+.btn-register {
+  background-color: rgba(179, 223, 250, 255);
+  color: aliceblue;
+  transition: 0.5s;
+}
+.btn-register:hover {
+  background-color: rgba(1, 149, 247, 255);
+  color: aliceblue;
+}
+.v-divider {
+  margin-left: 10px;
+  margin-right: 10px;
+}
+.v-text-field {
+  display: flex;
+}
+input {
+  border: solid rgba(216, 216, 216, 255) 1.3px;
+  border-radius: 0.3rem;
+  margin: 2px;
+  background-color: rgba(250, 250, 250, 255);
+  width: 14rem;
+  height: 2rem;
+  font-size: 12px;
+  padding-left: 0.7rem;
+  color: rgb(33, 33, 33);
+}
+.input-form {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+img {
+  width: 3rem;
+}
+</style>
